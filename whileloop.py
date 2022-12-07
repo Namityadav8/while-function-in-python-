fbafd43a42233f480085284f94cfaf97f4db296e
Namit Yadav <114800158+Namityadav8@users.noreply.github.com>
2022-12-08 00:13:02 +0530

    while loop function in python

---
+++ whileloop.py
@@ -0,0 +1,9 @@
+# to print a number from 1 to 10 or break it at any particular number 
+i=1
+while i<10:
+   print(i)
+   i+=1
+else:
+    print("i is greater then 10")   
+for i in range(6):
+    print(i)
